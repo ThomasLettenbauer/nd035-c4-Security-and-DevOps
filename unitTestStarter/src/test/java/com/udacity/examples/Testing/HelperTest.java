90d104024ee9931f8212c99ce767f8bdd0ebcead
thomas <thomas.lettenbauer@gmail.com>
2021-08-04 20:37:36 +0200

    test

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,33 @@
 package com.udacity.examples.Testing;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
 
-	
+
+
+    @Test
+    public void testGetCount() {
+        List<String> empNames = Arrays.asList("Thomas", "Sabine");
+        final long actual = Helper.getCount(empNames);
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void verify_getMergedList() {
+        List<String> names = Arrays.asList("Thomas", "Sabine", "Timo");
+        String actual = Helper.getMergedList(names);
+        assertEquals("Thomas, Sabine, Timo", actual);
+            }
+
+    //public static String getMergedList(List<String> empName) {
+    //    return empName.stream().filter(string ->!string.isEmpty()).collect(Collectors.joining(", "));
+
 }
